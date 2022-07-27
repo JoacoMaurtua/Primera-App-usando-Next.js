c7e12b643cacfb26e2a0e74c93da7d70146c3d96
JoacoMaurtua <joacomaurtua@gmail.com>
2022-07-27 11:52:06 -0500

    archivos de next y iniciando rutas basadas en archivos

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+//import React from 'react' --> ESTO YA NO ES NECESARIO
+
+const index = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>Aprendamos Next.js</p>
+    </div>
+  )
+}
+
+export default index
