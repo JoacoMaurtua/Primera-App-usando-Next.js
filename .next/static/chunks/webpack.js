3a690987d049b07250e929880fa770fc3ce5ed95
JoacoMaurtua <joacomaurtua@gmail.com>
2022-07-29 12:32:17 -0500

    extrayendo la data del API con axios en posts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c855b61dbf207967"; }
+/******/ 		__webpack_require__.h = function() { return "93723066dced270d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import Navbar from '../components/navbar';
+import React, {useState, useEffect} from 'react'
 import Title from '../components/title';
 import Layout from '../components/layout';
+import axios from 'axios';
+import Link from 'next/link';
 
-const users = () => {
+const posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(data);
+    }
+    fetchPosts();
+  },[]);
+
+  console.log('posts:',posts);
+  
   return (
     <Layout>
       <Title>Post Page</Title>
+      <div>
+        {posts.map((post) => {
+          return(
+            <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
+              <a>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </a>
+            </Link>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
 
-export default users
+export default posts
